7cefd17efad59e515637cf582d0d9bc845651136
Baxi19 <randald1991@gmail.com>
2021-04-21 11:59:01 -0600

    :fire: Test sockets conection

--- Nodo_Principal/scraper.py
+++ Nodo_Principal/scraper.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
-
+from socket_client import Socket_Client
 
 def parse(page):
     url = 'https://store.playstation.com/es-cr/category/d71e8e6d-0940-4e03-bd02-404fc7d31a31/' + str(page)
@@ -15,9 +15,9 @@ def parse(page):
     driver.get(url)
     games = driver.find_elements_by_xpath('/html/body/div[3]/main/section/div/div/ul/li')
 
-    time.sleep(2)
-
-    json_list = []
+    time.sleep(3)
+    ps5_list = []
+    
     for item in games:
         # url
         game_url = item.find_element_by_xpath('.//div[@class="ems-sdk-product-tile"]/a/div/div/span[2]/img').get_attribute('src')
@@ -33,12 +33,13 @@ def parse(page):
             "name": game_name,
             "price":game_price,
             "store":"PlayStation",
-            "url":modify_url[0]
+            "url": modify_url[0]
         }
-        json_list.append(newGame)
+
+        ps5_list.append(newGame)
         
     driver.close()
-    return json_list
+    return ps5_list
 
 
 def get_ps5_games():
@@ -55,6 +56,14 @@ def get_ps5_games():
         print(game['store'])
         print(game['url'])
             
+
+    
+    # Test
+    client = Socket_Client("localhost",10000, b'Hello, Im Node 1')
+    client.send()
+
+    client2 = Socket_Client("localhost",11000, b'Hello, Im Node 1')
+    client2.send()
 
 if __name__ == "__main__":
     get_ps5_games()
