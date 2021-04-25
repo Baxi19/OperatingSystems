186a260438186800c711119cdf46b78ecf5f3a9e
Jazmine <jaz11600@gmail.com>
2021-04-24 20:31:54 -0600

    Compare price

--- Nodo_Secundario_1/search_games.py
+++ Nodo_Secundario_1/search_games.py
@@ -3,7 +3,8 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
 
-def search(name):
+# Search game in Amazon
+def search(name, price):
     url = 'https://www.amazon.com/s?i=videogames-intl-ship&bbn=16225016011&rh=n%3A20972781011%2Cn%3A20972797011%2Cp_89%3APlaystation&dc&language=es&fst=as%3Aoff&pf_rd_i=16225016011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=e963b29d-d654-4995-bbb2-582c8cfbb5e4&pf_rd_r=0E6CBDMYSY2Z4QW1EYKE&pf_rd_s=merchandised-search-3&pf_rd_t=101&qid=1619289230&rnid=20972781011&ref=sr_nr_n_3'
 
     # Selenium
@@ -26,15 +27,22 @@ def search(name):
     # Amazon changes the search category when it can't find the item
     searchCategory = driver.find_element_by_id('nav-search-label-id')
 
-    # Item
-    price_whole = driver.find_element_by_xpath('//*[@id="search"]/div[1]/div/div[1]/div/span[3]/div[2]/div[1]/div/span/div/div/div[2]/div[2]/div/div[2]/div[1]/div/div[1]/div[2]/a/span/span[2]/span[2]')
-    price_fraction = driver.find_element_by_xpath('//*[@id="search"]/div[1]/div/div[1]/div/span[3]/div[2]/div[1]/div/span/div/div/div[2]/div[2]/div/div[2]/div[1]/div/div[1]/div[2]/a/span/span[2]/span[3]')
-
     if (searchCategory.text != "Juegos de PlayStation 5"):
         print("No se encontro el elemento solicitado")
     else:
-        print(price_whole.text)
-        print(price_fraction.text)
+        # Price
+        price_find_element = driver.find_element_by_xpath('//*[@class="a-offscreen"]')
+        price_find = price_find_element.get_attribute('innerText')
+        
+        price_find = price_find.split("US$\xa0")[1]
+        price = price.split("US$")[1]
+        return compare_price(price, price_find)
 
+def compare_price(actual_price, price_find):
+    if (actual_price <= price_find):
+        return actual_price
+    else:
+        return price_find
 
-search("Demon's Souls")
+better = search("Demon's Souls","US$89.99")
+print(better)
